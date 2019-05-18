e5fb5e338ef04b10bbb3cde62b0bb5f715ee1596
Alberto Rodriguez <juniorrodriguez@Albertos-iMac.local>
2019-05-18 17:26:34 -0500

    added props.toDoList

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Todo = function (props) {
     return(
-
+        <div className="toDo">
+            <div className="toDoList">
+                <h3>props.toDoList.task</h3>
+            </div>
+        </div>
     );
 };
 
